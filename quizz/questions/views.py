1be55a9be98ede15ea031f8d09e75882493fc4e0
eromanv <e.roman.v@ya.ru>
2023-05-25 12:34:45 -0300

    added requiremets

--- quizz/questions/views.py
+++ quizz/questions/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Question_quiz
 from .serializers import QuestionSerializer
 
+
 def recieve_question(request, id):
-    if request.method == 'GET':
+    if request.method == "GET":
         get_question = Question_quiz.objects.get(id=id)
         serializer = QuestionSerializer(get_question).data
         return JsonResponse(serializer)
-        
