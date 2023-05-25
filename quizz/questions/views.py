a4c79174056d6d20d5c5506fdac1bb99db909ac1
eromanv <e.roman.v@ya.ru>
2023-05-25 12:20:37 -0300

    added serializers

--- quizz/questions/views.py
+++ quizz/questions/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Question_quiz
+from .serializers import QuestionSerializer
 
-def get_question(request, pk):
+def recieve_question(request, id):
     if request.method == 'GET':
+        get_question = Question_quiz.objects.get(id=id)
+        serializer = QuestionSerializer(get_question).data
+        return JsonResponse(serializer)
         
