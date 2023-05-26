4ceaa87ffbfb4665b4192b70d5854308f1287e0e
eromanv <e.roman.v@ya.ru>
2023-05-25 22:00:03 -0300

    added api

--- quizz/questions/views.py
+++ quizz/questions/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 import requests
 from rest_framework.views import APIView
-from rest_framework.response import Response, 
+from rest_framework.response import Response
 
 from .models import Question_quiz
 from .serializers import QuestionSerializer
