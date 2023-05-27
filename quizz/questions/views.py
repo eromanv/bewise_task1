923956b644249521d0185bbdc25c16f4e9a0be14
eromanv <e.roman.v@ya.ru>
2023-05-26 23:25:15 -0300

    finally done with postgres

--- quizz/questions/views.py
+++ quizz/questions/views.py
@@ -1,8 +1,9 @@
 from django.http import JsonResponse
 import requests
 from rest_framework.views import APIView
-from rest_framework.response import Response, g
+from rest_framework.response import Response
 import psycopg2
+from psycopg2 import Error
 import os
 from .models import Question_quiz
 from .serializers import QuestionSerializer
@@ -23,25 +24,35 @@ class PostQuestions(APIView):
         )
         if response.status_code == 200:
             data = response.json()
-
             conn = psycopg2.connect(
-                dbname=os.getenv('DB_NAME'),
-                user=os.getenv('DB_USER'),
-                password=os.getenv('DB_PASSWORD'),
-                host=os.getenv('DB_HOST'),
-                port=os.getenv('DB_PORT')
+                user="postgres",
+                password="postgres",
+                host="localhost",
+                port="5432",
+                database="postgres"
             )
             cursor = conn.cursor()
-            cursor.execute("CREATE TABLE IF NOT EXISTS questions (id serial PRIMARY KEY, question varchar, answer varchar, created_at timestamp);")
+            create_table_query = '''CREATE TABLE questions
+                (ID SERIAL PRIMARY KEY     NOT NULL,
+                QUESTION           TEXT    NOT NULL,
+                ANSWER             TEXT    NOT NULL,
+                CREATED_AT         TIMESTAMP);'''
+
+           
+            cursor.execute(create_table_query)
+            conn.commit()
+            print("Table created successfully")
             for item in data:
                 id = item.get('id')
                 question = item.get('question')
                 answer = item.get('answer')
                 created_at = item.get('created_at')
-                cursor.execute("INSERT INTO questions (id, question, answer, created_at) VALUES (%s, %s, %s, %s)", (id, question, answer, created_at))
+                cursor.execute("INSERT INTO questions (ID, QUESTION, ANSWER, CREATED_AT) VALUES (%s, %s, %s, %s)", (id, question, answer, created_at))
             conn.commit()
             cursor.close()
             conn.close()
+            return Response(data, status=status.HTTP_200_OK)
+
         else:
             return Response(
                 {"error": "Failed to fetch questions"}, status=response.status_code
