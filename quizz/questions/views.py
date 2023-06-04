2218b207cb38c131f1eddc0c9ecda9bd047ca77c
eromanv <e.roman.v@ya.ru>
2023-06-04 06:57:34 -0300

    added the return of none object, and added the previous object

--- quizz/questions/views.py
+++ quizz/questions/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 import psycopg2
 from psycopg2 import Error
 import os
@@ -32,7 +33,7 @@ class PostQuestions(APIView):
                 database="postgres"
             )
             cursor = conn.cursor()
-            create_table_query = '''CREATE TABLE questions
+            create_table_query = '''CREATE TABLE IF NOT EXISTS questions
                 (ID SERIAL PRIMARY KEY     NOT NULL,
                 QUESTION           TEXT    NOT NULL,
                 ANSWER             TEXT    NOT NULL,
@@ -42,17 +43,32 @@ class PostQuestions(APIView):
             cursor.execute(create_table_query)
             conn.commit()
             print("Table created successfully")
-            for item in data:
-                id = item.get('id')
-                question = item.get('question')
-                answer = item.get('answer')
-                created_at = item.get('created_at')
-                cursor.execute("INSERT INTO questions (ID, QUESTION, ANSWER, CREATED_AT) VALUES (%s, %s, %s, %s)", (id, question, answer, created_at))
-            conn.commit()
+            unique_questions = set()
+            previous_question = None
+            while len(unique_questions) < questions_num:
+                response = requests.get(
+                    "https://jservice.io/api/random", params={"count": questions_num}
+                )
+                if response.status_code == 200:
+                    data = response.json()
+                    for item in data:
+                        question = item.get('question')
+                        if question not in unique_questions:
+                            unique_questions.add(question)
+                            id = item.get('id')
+                            answer = item.get('answer')
+                            created_at = item.get('created_at')
+                            cursor.execute("INSERT INTO questions (ID, QUESTION, ANSWER, CREATED_AT) VALUES (%s, %s, %s, %s)", (id, question, answer, created_at))
+                            conn.commit()
+                            previous_question = {
+                                'id': id,
+                                'question': question,
+                                'answer': answer,
+                                'created_at': created_at
+                                }
             cursor.close()
             conn.close()
-            return Response(data, status=status.HTTP_200_OK)
-
+            return Response(previous_question, status=status.HTTP_200_OK)
         else:
             return Response(
                 {"error": "Failed to fetch questions"}, status=response.status_code
